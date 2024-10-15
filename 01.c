ccd1e978ef643980448ea71df75b734f4bc4d172
damgin <ant529@outlook.fr>
2024-10-15 09:22:05 +0200

    update

--- 01.c
+++ 01.c
@@ -6,17 +6,30 @@
 #define PILE 1
 
 int main(){
+    
     printf  ("Règles du Jeux : entrée (1) pour pile et (2) pour face\n pour quitter entrée (0)\n");
     srand(time(NULL));
-    while(1){
+    int boucle = 1;
+    while(boucle < 11){
 
         int piece = rand()%2 + 1;     ///aléatoire 1//2 = pile ou face
+        printf("boucle: %d\n",boucle);
         printf ("pile ou Face ?\n"); 
+        boucle++;
             
             
         int reponse;             ////on atribut un réponse qui devra etre correct///
-        
         scanf("%d",&reponse);
+        printf("la réponse est");
+        fflush(stdout);
+        sleep(1);
+        printf(".");
+        fflush(stdout);
+        sleep(1);
+        printf(".");
+        fflush(stdout);
+        sleep(1);
+        printf(".:\n");
         if(reponse == 0){
             printf("a bientot !\n");
             return 0;
